09a9bb38cae9730f30a026592aeb5cb4447fb9e4
kmurban <kcnjhnsn@gmail.com>
2018-02-05 22:17:25 -0800

    Initial commit

---
+++ js/cute-animals.js
@@ -0,0 +1,14 @@
+let srcString = "";
+let imgNumber = 1;
+
+function createSnuggle() {
+	imgNumber = Math.floor(Math.random() * 10) + 1;
+	srcString = "images/" + String(imgNumber) + ".jpg";
+	console.log(srcString);
+}
+
+$('.summon-button').click(function(){
+	createSnuggle();
+	$('.photo').attr('src',srcString);
+	$(this).text("Summon another snuggle");
+});
